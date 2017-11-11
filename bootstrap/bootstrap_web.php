514f22689a67a43e7f7a8b2daae2fdd8000a1084
Wellming Li <wellming.li@howzhi.com>
2017-11-11 21:47:06 +0800

    配置文件重命名

--- bootstrap/bootstrap_web.php
+++ bootstrap/bootstrap_web.php
@@ -4,7 +4,7 @@ use Codeages\PhalconBiz\Application;
 
 $biz = require __DIR__.'/bootstrap_biz.php';
 
-$config = require ROOT_DIR.'/config/app.php';
+$config = require ROOT_DIR.'/config/web.php';
 $app = new Application($biz, $config);
 
 return $app;
