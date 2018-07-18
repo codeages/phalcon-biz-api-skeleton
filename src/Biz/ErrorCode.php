1e7f1a52af74c4dc6f751611f42dc00f209c98a2
Wellming Li <wellming.li@howzhi.com>
2018-07-18 22:49:18 +0800

    add base error code

--- src/Biz/ErrorCode.php
+++ src/Biz/ErrorCode.php
@@ -2,12 +2,14 @@
 
 namespace App\Biz;
 
+use Codeages\PhalconBiz\ErrorCode as BaseErrorCode;
+
 /**
  * 业务错误码
  *
  * 约定业务错误码的 Code 为100以上，通用错误码为100以下，
  * 通用错误码见`Codeages\PhalconBiz\ErrorCode`。
  */
-class ErrorCode
+class ErrorCode extends BaseErrorCode
 {
 }
